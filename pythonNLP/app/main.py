152275c2a1e15302af60dd36b3372f7c852bf832
DikonFace <96048863+DikonFace@users.noreply.github.com>
2022-06-08 17:41:29 +0600

    Add files via upload

---
+++ pythonNLP/app/main.py
@@ -0,0 +1,23 @@
+import flask
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from flask import request
+app = flask.Flask(__name__)
+
+tokenizer = GPT2Tokenizer.from_pretrained('sberbank-ai/rugpt3medium_based_on_gpt2')
+model = GPT2LMHeadModel.from_pretrained('sberbank-ai/rugpt3medium_based_on_gpt2')
+
+@app.route('/', methods=['GET'])
+def home():
+    return 'api working'
+
+@app.route('/talk/', methods=['POST'])
+def talk():
+    if request.form.__getitem__('text'):
+        # initialize tokenizer and model from pretrained GPT2 model
+        inputs = tokenizer.encode(request.form.__getitem__('text'), return_tensors='pt')
+        outputs = model.generate(inputs, max_length=200, do_sample=False)
+        text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return text
+    else: 
+        return '500'
